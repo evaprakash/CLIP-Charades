730259b5adf241fb380c5866e515fa787eab17c3
Eva Prakash <evaprakash2@gmail.com>
2022-05-14 18:37:01 -0700

    Added script to split videos into frames

---
+++ create_frames.py
@@ -0,0 +1,62 @@
+import json
+import pickle
+import cv2
+import os
+import csv
+import numpy as np
+
+N = 100
+V = 20
+DATA_PATH = 'Charades_v1_480/'
+SAVE_PATH = 'test_frames/'
+CAPTIONS_FILE = 'Charades/Charades_v1_test.csv'
+
+# Function to extract frames
+def frame_capture(v_id):
+    # Path to video file
+    vidObj = cv2.VideoCapture(DATA_PATH + '%s.mp4' % v_id)
+
+    # Used as counter variable
+    count = 0
+
+    # Checks whether frames were extracted
+    success, image = vidObj.read()
+    
+    while success:
+
+        if count % N == 0:
+    
+            frame_url = '%s-%s.png' % (v_id, count)
+
+            # Saves the frames with frame-count
+            cv2.imwrite(SAVE_PATH + frame_url, image)
+
+        count += 1
+        
+        # vidObj object calls read
+        # function to extract frames
+        success, image = vidObj.read()
+
+    print("Done ", v_id)
+
+captions = {}
+
+#Consolidate captions for V videos
+with open(CAPTIONS_FILE, 'r') as file:
+    reader = csv.reader(file)
+    i = 0
+    for row in reader:
+        if (i == 0):
+            i += 1
+            continue
+        captions[row[0]] = row[6]
+        if (i == 20):
+            break
+        i += 1
+
+with open(SAVE_PATH + 'captions.json', 'w') as f:
+    json.dump(captions, f)
+
+#Create frame images and image data
+for v_id in captions:
+    frame_capture(v_id)
